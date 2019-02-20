8d512e443b2406064170840481e1afbf0ce09b43
lauri <lauri.kukk1@gmail.com>
2019-02-20 19:18:48 +0200

    added map

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyDqUg6REy00DK0SMufqPULw0o7N5VjTyPU',
+        libraries: 'places' // This is required if you use the Autocomplete plugin
+        // OR: libraries: 'places,drawing'
+        // OR: libraries: 'places,drawing,visualization'
+        // (as you require)
+
+        //// If you want to set the version, you can do so:
+        // v: '3.26',
+    }
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
+
