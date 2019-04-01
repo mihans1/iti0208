e7e0fd40af577bd5781ba8dc4ba4ff86f2781818
Kaidi <kaidi.g@gmail.com>
2019-04-01 21:01:00 +0300

    addobject postreq is ok

--- client/src/components/AddObject.vue
+++ client/src/components/AddObject.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="a" @load="getObjectTypes">
         <add/>
-        <v-form id="obj-add" method="post" @submit="addObject">
+        <v-form id="obj-add" method="post" @submit="">
             <div id="type">
                 <label for="obj-type"><h3>Choose obstacle type</h3></label>
                 <v-select id="obj-type" name="objectType" v-model="objectType"
@@ -13,15 +13,32 @@
                 <label for="loc"><h3>Pick obstacle location</h3></label>
                 <p/>
                 <area alt="o" id="loc">
-                Siin voiks pisike map olla??? <p/>
                 <v-text-field
                         value="Description"
                         hint="For example a car on the pavement or no passing"
                 ></v-text-field>
+                <div class="columns">
+                    <div class="column">
+                        <label class="label" for="class-lat" >Latitude</label>
+                        <p class="control"><input id="class-lat"
+                                                  class="input"
+                                                  type="number"
+                                                  pattern="[0-9]{2}.[0-9]{6}"
+                                                  name=""
+                                                  v-model="lat" ></p>
+                    </div>
+                    <div class="column">
+                        <label class="label" for="class-lng">Longitude</label>
+                        <p class="control"><input id="class-lng" class="input" pattern="[0-9]{2}.[0-9]{6}" type="text" name="" v-model="lng" ></p>
+                    </div>
+                </div>
+                <coordinate-input :drag-marker="true" @select="addLocation" :lat="lat" :lng="lng"
+                                  lat-name="lat-name" lng-name="lng-name" api-key=""/>
+                <button type="submit" v-on:click="addLocation">Add obstacle</button>
 
                 <input type="file"/>
-                <p/>
-                <button v-on:click.prevent="addObject">Add obstacle</button>
+
+<!--                <button type="submit" v-on:click.prevent="addObject">Add obstacle</button>-->
             </div>
         </v-form>
     </div>
@@ -29,6 +46,7 @@
 <script>
 
 
+    import CoordinateInput from "@lassehaslev/vue-coordinate-input/dist/components/CoordinateInput";
     const add = {
         render(h) {
             return h('div', [
@@ -40,12 +58,15 @@
     export default {
         name: "hhh",
         components: {
+            CoordinateInput,
             add
         },
         data() {
             return {
                 objectTypes: {},
-                objectType: null
+                objectType: null,
+                lat: 0,
+                lng: 0
             }
         },
         methods: {
@@ -54,16 +75,25 @@
                     this.objectTypes = response.body;
                 })
             },
-            addObject: function () {
-                this.$http({method: 'POST', url: 'http://localhost:8080/api/object'})
+            addLocation: function() {
+                this.$http({method: 'POST', url: 'http://localhost:8080/api/locations'})
                     .then(function (response) {
-                        this.object = response.body;
+                        response.send(
+                            this.lat = Number(response.lat),
+                            this.lng = response.lng
+                        )
                     })
-            }
+            },
+            // addObject: function () {
+            //     this.$http({method: 'POST', url: 'http://localhost:8080/api/objects'})
+            //         .then(function (response) {
+            //             this.object = response.body;
+            //         })
+            // }
         },
         mounted() {
             this.getObjectTypes();
-            this.addObject();
+            // this.addObject();
         }
     }
 
